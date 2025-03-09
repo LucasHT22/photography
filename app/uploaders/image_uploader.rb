211b090c16b466f5ae558bd520479917ea6e6e6d
Lucas Honda <88567622+LucasHT22@users.noreply.github.com>
2025-03-09 02:07:13 +0000

    try fix

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,9 +7,9 @@ class ImageUploader < CarrierWave::Uploader::Base
     'uploads/photos'
   end
 
-  def extension_whitelist
+  def extension_allowlist
     %w[jpg jpeg gif png]
-  end
+  end  
 
   version :thumb do
     process resize_to_fit: [200, 200]
