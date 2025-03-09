bcbc6b60c3d738cb42dd909be7fa63ddf1df0870
Lucas Honda <88567622+LucasHT22@users.noreply.github.com>
2025-03-09 18:05:30 +0000

    updates & fixes

--- Gemfile
+++ Gemfile
@@ -2,6 +2,11 @@ source "https://rubygems.org"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 8.0.1"
+
+gem 'carrierwave', '~> 2.0'
+
+gem 'mini_magick', '~> 4.0'
+
 # The modern asset pipeline for Rails [https://github.com/rails/propshaft]
 gem "propshaft"
 # Use sqlite3 as the database for Active Record
@@ -17,8 +22,7 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
-gem 'carrierwave', '~> 2.0'
-gem 'mini_magick'
+
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,17 @@
 image_dir = Rails.root.join('public', 'photos')
 
 if Dir.exist?(image_dir)
-  image_files = Dir[image_dir.join('*.{jpg,jpeg,png}')]
+  image_files = Dir[image_dir.join('*.{jpg,jpeg,png,gif}')]
 
   image_files.each do |image_path|
     photo = Photo.new(
-      title: File.basename(image_path, '.*'),
-      description: "Descrição para #{File.basename(image_path, '.*')}",
       image: File.open(image_path)
     )
+
     if photo.save
-      puts "Photo '#{photo.title}' added successfully!"
+      puts "Photo '#{photo.title}' addded successfully!"
     else
-      puts "Error adding '#{photo.title}'"
+      puts "Error adding '#{photo.title}': #{photo.errors.full_messages.join(", ")}"
     end
   end
 else
